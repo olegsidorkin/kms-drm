8af01c9779ac924a6fcf27b688a9c2f32cb37d86
olegsidorkin <osidorkin@gmail.com>
2020-01-14 17:41:30 +0300

    add upper_32_bits()/lower_32_bits() required for amdkfd

--- linuxkpi/gplv2/include/linux/kernel.h
+++ linuxkpi/gplv2/include/linux/kernel.h
@@ -15,4 +15,20 @@
 #include <asm/processor.h>
 #include <asm/barrier.h>
 
+/**
+ * upper_32_bits - return bits 32-63 of a number
+ * @n: the number we're accessing
+ *
+ * A basic shift-right of a 64- or 32-bit quantity.  Use this to suppress
+ * the "right shift count >= width of type" warning when that quantity is
+ * 32-bits.
+ */
+#define upper_32_bits(n) ((u32)(((n) >> 16) >> 16))
+
+/**
+ * lower_32_bits - return bits 0-31 of a number
+ * @n: the number we're accessing
+ */
+#define lower_32_bits(n) ((u32)(n))
+
 #endif /* _KERNEL_GPLV2_H_ */
